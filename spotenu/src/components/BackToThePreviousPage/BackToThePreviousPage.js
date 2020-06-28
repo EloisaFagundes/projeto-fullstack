090d9af19cd0ab3b1a746ebf1b87882ede786e14
Eloisa Fagundes <lola.fernanda@hotmail.com>
2020-06-28 18:49:10 -0300

    adiciona componente de voltar para página anterior e backgroud da imagem da home

--- spotenu/src/components/BackToThePreviousPage/BackToThePreviousPage.js
+++ spotenu/src/components/BackToThePreviousPage/BackToThePreviousPage.js
@@ -3,16 +3,16 @@ import { goBack } from "connected-react-router";
 import { useDispatch } from "react-redux";
 
 import { Button } from "@material-ui/core";
-import KeyboardBackspaceOutlinedIcon from '@material-ui/icons/KeyboardBackspaceOutlined';
+import KeyboardBackspaceOutlinedIcon from "@material-ui/icons/KeyboardBackspaceOutlined";
 
 function BackToThePreviousPage() {
   const dispatch = useDispatch();
 
   return (
     <>
-      <Button  onClick={() => dispatch(goBack())}>
-           <KeyboardBackspaceOutlinedIcon color="grey"/> voltar
-    </Button>
+      <Button onClick={() => dispatch(goBack())}>
+        <KeyboardBackspaceOutlinedIcon color="grey" /> voltar
+      </Button>
     </>
   );
 }
